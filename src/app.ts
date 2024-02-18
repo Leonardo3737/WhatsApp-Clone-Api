8385dc68af076d592f889ef44f8a545b738ca43c
Leonardo3737 <leovinicius3737@gmail.com>
2024-02-18 00:13:21 -0400

    alterar metodo de autenticação

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
-require('dotenv').config();
-import { Chat, Message } from 'whatsapp-web.js';
+import { Chat, LocalAuth, Message } from 'whatsapp-web.js';
 import { Chats, Messages } from './interfaces/index'
 import { Http2SecureServer } from 'http2';
-import { Socket } from 'dgram';
-import { log } from 'console';
-const { Client, RemoteAuth } = require('whatsapp-web.js');
+import { Socket } from 'socket.io';
+const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-
-const { MongoStore } = require('wwebjs-mongo');
 const mongoose = require('mongoose');
 
 const express = require('express');
@@ -26,98 +22,95 @@ const corsOptions = {
 
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const httpServer: Http2SecureServer= createServer(app);
+const httpServer: Http2SecureServer = createServer(app);
 
 const io = new Server(httpServer, { cors: corsOptions });
 
 app.use(cors([corsOptions]))
 app.use(express.json());
-
-mongoose.connect(process.env.MONGO_DB_URI).then(() => {
-  const store = new MongoStore({ mongoose: mongoose });
-  const client = new Client({
-    authStrategy: new RemoteAuth({
-      store: store,
-      backupSyncIntervalMs: 300000
-    })
-  });
-
-  client.on('ready', async () => {
-    console.log('client is ready');
-  });
-
-  client.on('remote_session_saved', () => {
-    console.log('conectado ao banco de dados');
-  });
-
-  client.on('qr', (qr: any) => {
-    qrcode.generate(qr, { small: true });
-  });
-
-  app.post('/sendMessage', async (req: any, res: any) => {
-    try {
-      const to = req.body.to, message = req.body.message
-      await client.sendMessage(to, message)
-      res.send('success')
-    } catch (err) {
-      console.log(err);
-      res.send(err)      
-    }
-  })
-
-  app.get('/getChats', async (req: any, res: any) => {
-    try {
-      const auxChats = await client.getChats()
-      const chats: Chats[] = auxChats.map((c: Chat) => {
-        return {
-          id: c.id._serialized,
-          name: c.name,
-          isGroup: c.isGroup,
-          lastMessage: c.lastMessage ? {
-            body: c.lastMessage.body,
-            type: c.lastMessage.type,
-            author: c.lastMessage.author,
-          } : null,
-          timestamp: c.timestamp
-        }
-      })
-      res.send(chats)
-    } catch (err) {
-      console.log(err);
-      res.send(err)
-    }
-  })
-
-  app.post('/getMessages', async (req: any, res: any) => {
-    try {
-      const chat = await client.getChatById(req.body.user)
-      const auxMessages = await chat.fetchMessages({ limit: 20 })
-      const messages: Messages[] = auxMessages.map((m: Message) => {
-        return {
-          timestamp: m.timestamp,
-          ack: m.ack,
-          author: m.from,
-          body: m.body,
-          fromMe: m.fromMe
-        }
-      })
-      res.send(messages)
-    } catch (err) {
-      res.send(err)
-    }
-  })
-
-  io.on('connection', (socket: any) => {
-    console.log('user connected');
-    client.on('message_create', (message: Message) => {
-      socket.emit("newMessage", message.body)   
-    })
-    console.log(socket.id);
-    
-  })
-
-  client.initialize();
-  httpServer.listen(port, () => {
-    console.log('servidor rodando');
-  })
+const client = new Client({
+  authStrategy: new LocalAuth()
 });
+
+client.on('ready', async () => {
+  console.log('client is ready');
+});
+
+client.on('remote_session_saved', () => {
+  console.log('session saved');
+});
+
+client.on('qr', (qr: any) => {
+  qrcode.generate(qr, { small: true });
+});
+
+app.post('/sendMessage', async (req: any, res: any) => {
+  try {
+    const to = req.body.to, message = req.body.message
+    await client.sendMessage(to, message)
+    res.send('success')
+  } catch (err) {
+    console.log(err);
+    res.send(err)
+  }
+})
+
+
+app.get('/getChats', async (req: any, res: any) => {
+  const auxChats = await client.getChats()
+  try {
+    const chats: Chats[] = auxChats.map((c: Chat) => {
+      return {
+        id: c.id._serialized,
+        name: c.name,
+        isGroup: c.isGroup,
+        lastMessage: c.lastMessage ? {
+          body: c.lastMessage.body,
+          type: c.lastMessage.type,
+          author: c.lastMessage.author,
+        } : null,
+        timestamp: c.timestamp
+      }
+    })
+    res.send(chats)
+  } catch (err) {
+    console.log(err);
+    res.send(err)
+  }
+})
+
+app.post('/getMessages', async (req: any, res: any) => {
+  try {
+    let auxChats: Chat[] | null = null
+    const chat:Chat = await client.getChatById(req.body.user)
+    if(chat.isGroup){
+      auxChats = await client.getChats()
+    }
+    const auxMessages = await chat.fetchMessages({ limit: 20 })
+    const messages: Messages[] = auxMessages.map((m: Message) => {
+      let aux: any = auxChats ? auxChats.find((c:Chat)=> c.id._serialized === m.author) : m.author
+      return {
+        timestamp: m.timestamp,
+        ack: m.ack,
+        author: aux ? aux.name : m.author,
+        body: m.body,
+        fromMe: m.fromMe
+      }
+    })
+    res.send([{isGroup: chat.isGroup}, messages])
+  } catch (err) {
+    res.send(err)
+  }
+})
+
+io.on('connection', (socket: Socket) => {
+  console.log('user connected');
+  client.on('message_create', (message: Message) => {
+    socket.emit("newMessage", message.body)
+  })
+})
+
+client.initialize();
+httpServer.listen(port, () => {
+  console.log('servidor rodando');
+})
